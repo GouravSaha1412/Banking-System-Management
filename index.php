3be17a9a3c0deebbb66a64822ff4bb446146a13f
bsajidul <bsajidul@gmail.com>
2021-12-11 00:31:21 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+    <head>
+        <title>Banking System Management</title>
+        <link href="CSS/style.css" rel="stylesheet" type="text/css">
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    </head>
+    <body class="bg">
+        <center>
+            <?php include 'header_html_main.html'; ?>
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <?php include 'footer.html'; ?>
+        </center>
+    </body>
+</html>
